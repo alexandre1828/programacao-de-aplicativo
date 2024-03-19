d59c6a87547dd435fb11c8f48eb4411c45fa3c5a
alexandre1828 <144128822+alexandre1828@users.noreply.github.com>
2024-03-19 09:30:51 -0300

    Create atv2.java

---
+++ atv2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+public static void main(String[] args) {
+    Scanner teclado = new Scanner (System.in);
+    
+
+System.out.println("digite a opcao");
+int opcao = teclado.nextInt();
+
+
+if (opcao == 1) {
+     
+ 
+  System.out.println("saldo R$ 5,00");
+
+}
+
+if (opcao == 2) {
+    
+    System.out.println("meu nome e alexandre");
+}
+
+
+if (opcao == 3) {
+    
+    System.out.println("transferir para o atendente");
+} 
+
+
+ 
+
+}
+}
