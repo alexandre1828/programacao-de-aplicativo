f70adbdb6950c5774d6ef7fb88b1e14a2ca4b7c9
alexandre1828 <144128822+alexandre1828@users.noreply.github.com>
2024-04-10 14:36:05 -0300

    Create par ou impar em for.java

---
+++ par ou impar em for.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+
+{
+	public static void main(String[] args) {
+	      Scanner teclado = new Scanner (System.in);
+	  for(int i = 1; i<=70;i++) {
+	      
+	      if (i %  2 != 0 ) {
+	           System.out.println(i);
+	      } 
+	  }
+	}
+}
